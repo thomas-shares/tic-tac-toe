9b83b4229438070e62f4814055e2b15c94cdfd0f
Thomas van der Veen <veent@uk.ibm.com>
2015-12-31 17:38:50 +0000

    updated to latest versions of everything.

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.typed "0.2.92"]
-                 [compojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
                  [hiccup "1.0.5"]
-                 [ring/ring-jetty-adapter "1.1.0"]
-                 [org.clojure/clojurescript "0.0-2727"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [org.clojure/clojurescript "1.7.189"]
                  [domina "1.0.3"]
                  [hiccups "0.3.0"]]
   :main ^:skip-aot tic-tac-toe.core
